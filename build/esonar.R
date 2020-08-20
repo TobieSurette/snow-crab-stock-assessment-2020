18ae7398d2ed1e7ff5260d98795267e13f7ea214
TobieSurette <tobie.surette@gmail.com>
2020-08-20 12:24:31 -0300

    add star oddi data

--- build/esonar.R
+++ build/esonar.R
@@ -1,15 +1,8 @@
 library(gulf.utils)
 library(gulf.data)
 
-
 source("C:/Users/SuretteTJ/Desktop/gulf.data/R/esonar.R")
 
-files <- locate.esonar(year = 2020)
+files <- locate.esonar(path = "W:/Crab/Offshore Crab Common/Fishing Year 2020/Trawl Data/South Western Gulf/ESonar/Summary", remove = FALSE)
 
-locate.esonar(year = 2020)
-
-locate.esonar(path = "W:/Crab/Offshore Crab Common/Fishing Year 2020/Trawl Data/South Western Gulf/ESonar/Summary")
-
-
-
-file.copy(from = files, to = "/data/esonar", overwrite = TRUE, copy.date = TRUE)
+file.copy(from = files, to = "data/esonar", overwrite = TRUE, copy.date = TRUE)
--- build/star.oddi.R
+++ build/star.oddi.R
@@ -1,33 +1,11 @@
 
-# Header files:
-files <- star.oddi.file.str(year = 2020)
-tows <- unlist(lapply(strsplit(toupper(files), "GP"), function(x) x[length(x)]))
-tows <- unlist(lapply(strsplit(tows, "/"), function(x) x[1]))
-tows <- unlist(lapply(strsplit(tows, " "), function(x) x[1]))
-tows <- paste0("GP", tows)
+files <- locate.star.oddi(path = "W:/Crab/Offshore Crab Common/Fishing Year 2020/Trawl Data/South Western Gulf/Star Oddi", keywords = "headline")
+tow.id <- substr(unlist(lapply(strsplit(files, "/"), function(x) x[length(x)-1])), 6, 15)
+file.copy(from = files, to = paste0("data/star oddi/headline/", tow.id, ".dat"), overwrite = TRUE, copy.date = TRUE)
 
-names <- unlist(lapply(strsplit(toupper(files), "/"), function(x) x[length(x)]))
+files <- locate.star.oddi(path = "W:/Crab/Offshore Crab Common/Fishing Year 2020/Trawl Data/South Western Gulf/Star Oddi", keywords = "tilt")
+tow.id <- substr(unlist(lapply(strsplit(files, "/"), function(x) x[length(x)-1])), 6, 15)
+file.copy(from = files, to = paste0("data/star oddi/headline/", tow.id, ".dat"), overwrite = TRUE, copy.date = TRUE)
 
-path <- "U:/Snow Crab/Stock Assessment 2020/data/star oddi/headline"
-for (i in 1:length(files)){
-   file.copy(from = files[i], to = path, overwrite = TRUE, copy.date = TRUE)
-   names <- unlist(lapply(strsplit(toupper(files[i]), "/"), function(x) x[length(x)]))
-   file.rename(from = paste0(path, "/", names), to = paste0(path, "/", tows[i], ".DAT"))
-}
 
-# Footrope files:
-files <- star.oddi.file.str(year = 2020, type = "tilt")
-tows <- unlist(lapply(strsplit(toupper(files), "GP"), function(x) x[length(x)]))
-tows <- unlist(lapply(strsplit(tows, "/"), function(x) x[1]))
-tows <- unlist(lapply(strsplit(tows, " "), function(x) x[1]))
-tows <- paste0("GP", tows)
-
-names <- unlist(lapply(strsplit(toupper(files), "/"), function(x) x[length(x)]))
-
-path <- "U:/Snow Crab/Stock Assessment 2020/data/star oddi/footrope"
-for (i in 1:length(files)){
-   file.copy(from = files[i], to = path, overwrite = TRUE, copy.date = TRUE)
-   names <- unlist(lapply(strsplit(toupper(files[i]), "/"), function(x) x[length(x)]))
-   file.rename(from = paste0(path, "/", names), to = paste0(path, "/", tows[i], ".DAT"))
-}
 
