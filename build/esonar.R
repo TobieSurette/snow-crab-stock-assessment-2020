288f6ea5aa44a60f445b2d742f790e1b4178fb8e
TobieSurette <tobie.surette@gmail.com>
2020-08-20 10:54:22 -0300

    Added data files

--- build/esonar.R
+++ build/esonar.R
@@ -1,4 +1,15 @@
+library(gulf.utils)
+library(gulf.data)
 
-files <- esonar.file.str(year = 2020)
 
-file.copy(from = files, to = "U:/Snow Crab/Stock Assessment 2020/data/esonar", overwrite = TRUE, copy.date = TRUE)
+source("C:/Users/SuretteTJ/Desktop/gulf.data/R/esonar.R")
+
+files <- locate.esonar(year = 2020)
+
+locate.esonar(year = 2020)
+
+locate.esonar(path = "W:/Crab/Offshore Crab Common/Fishing Year 2020/Trawl Data/South Western Gulf/ESonar/Summary")
+
+
+
+file.copy(from = files, to = "/data/esonar", overwrite = TRUE, copy.date = TRUE)
