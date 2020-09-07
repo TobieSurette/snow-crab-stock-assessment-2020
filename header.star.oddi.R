fa85b52ff6ec84b11acc9d63ffa9ec4df43fe70a
Tobie Surette <tobie.surette@gmail.com>
2020-09-07 15:11:29 -0300

    Move Star Oddi class to gulf.data

--- header.star.oddi.R
+++
@@ -1,8 +0,0 @@
-header.star.oddi <- function(x){
-   # HEADER.STAR.ODDI - Returns a Star Oddi object's header information.
-
-   fields <- setdiff(names(attributes(x)), names(attributes(data.frame())))
-   V <- attributes(x)[fields]
-
-   if (length(V) == 0) return(NULL) else return(V)
-}
--- start.time.gulf.set.R
+++
@@ -1,6 +0,0 @@
-start.time.gulf.set <- function(x){
-   # START.TIME.GULF.SET - Returns the trawl start time associated with an 'gulf.set' object.
-
-   return(time(x))
-}
-
