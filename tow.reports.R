84826207ab7c2d86f198b44da4430d8bf3b1ad9b
TobieSurette <tobie.surette@gmail.com>
2020-10-08 13:23:44 -0300

    Photo corrections

--- tow.reports.R
+++ tow.reports.R
@@ -6,5 +6,17 @@ library(gulf.spatial)
 x <- read.scsset(2020, valid = 1)
 
 # Plot tow data:
-plot(x, pdf = TRUE)
+tow.id <- "GP237F"
+clg(); plot(x[which(x$tow.id == "GP098F"), ])
+
+clg(); plot(x[which(x$tow.id == "GP002F"), ])
+
+clg(); plot(x, wingspread = FALSE, headline = FALSE, tilt = FALSE)
+
+
+read.esonar(x[which(x$tow.id == "GP276F"), ])
+
+clg()
+x <- read.scsset(2020, tow.id = "GP276F")
+plot(x)
 
