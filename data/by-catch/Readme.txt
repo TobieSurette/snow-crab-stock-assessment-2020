1bffba952c806bf7407312868085b8f039e250ad
Tobie Surette <tobie.surette@gmail.com>
2020-10-16 07:29:11 -0300

    Hierarchical swept area model

--- data/by-catch/Readme.txt
+++ data/by-catch/Readme.txt
@@ -1,5 +1,5 @@
-SCS2019L
-year - year of sampling
+scs.len.2020.csv
+date - Sampling date
 tow.id - Tow unique identifier within year
 species - species code from (Losier, R.J. and L.E. Waite. 1989. Systematic listing of scientific and/or common names
 of invertebrates, vertebrates and marine plants and their respective codes used by
@@ -9,17 +9,15 @@ length - length of animal, for fish it is fork length, for herring it is possibl
 length.unit - cm or mm
 length.precision - e.g. a length measurement of 17 cm with a precision of 1 means that the length is within 16.5 to 17.5 cm
 
-SCS2019C
-year - year of sampling
-Month - month of sampling
-Day - day of sampling
+scs.cat.2020.csv
+date - Sampling date
 tow.number - tow number for the date, e.g. first tow of the day is tow.number 1, next one is tow.number 2 and so on.
 tow.id - Tow unique identifier within year
 species - species code from (Losier, R.J. and L.E. Waite. 1989. Systematic listing of scientific and/or common names
 of invertebrates, vertebrates and marine plants and their respective codes used by
 Marine Fish Division, Fisheries and Oceans, Scotia-Fundy Region (revised). Can.
 Data Rep. Fish. Aquat. Sci. 721: 139 p.)
-number.caught - count of animals	
+number.caught - count of animals
 weight.caught - weight of animals caught in kg
 presence - presence of the species = 1; absence of the species = 0
-comment - 
+comment -
---
+++ programs/swept area/swept.area.cpp
@@ -0,0 +1,116 @@
+#include <TMB.hpp>
+
+template<class Type>
+Type objective_function<Type>::operator()(){
+   // Data declarations:
+   DATA_IVECTOR(t);           // Time index (n_obs).
+   DATA_VECTOR(width);        // Trawl width observations (n_obs).
+   DATA_IVECTOR(tow);         // Tow index (n_obs).
+   DATA_MATRIX(speed);        // Vessel speed matrix (n x n_tow).
+   DATA_VECTOR(x);            // Sampling station x coordinate.
+   DATA_VECTOR(y);            // Sampling station y coordinate.
+
+   // Parameter and random effect declarations:
+   PARAMETER(alpha);                    // Global mean parameter.
+   PARAMETER_VECTOR(tow_effect);        // Tow random intercept effect (n_tow).
+   PARAMETER(log_sigma_process_tow);    // Log-scale tow effect process error.
+   PARAMETER(log_sigma_obs_tow);        // Log-scale tow effect observation error.
+   PARAMETER(log_a_tow);                // Tow effect correlation smoothness parameter.
+   PARAMETER(log_rho_tow);              // Tow effect correlation range parameter.
+   PARAMETER(log_sigma_process_global); // Log-scale global process standard error.
+   PARAMETER(log_sigma_process);        // Log-scale process standard error.
+   PARAMETER(log_sigma_obs);            // Log-scale observation standard error.
+   PARAMETER(log_sigma_outlier);        // Log-scale outlier standard error.
+   PARAMETER(log_rho);                  // Log-scale range correlation parameter.
+   PARAMETER(logit_p_outlier);          // Logit-scale proportion of outliers (n_tow).
+   PARAMETER_VECTOR(lambda_global);     // Global Autoregressive random effect (n).
+   PARAMETER_MATRIX(lambda);            // Autoregressive random effect (n x n_tow).
+   PARAMETER(log_a);                    // Gaussian process rational quadratic smoothness parameter.
+
+   // Transformed parameters:
+   Type sigma_process_tow    = exp(log_sigma_process_tow);    // Tow effect process error.
+   Type sigma_obs_tow        = exp(log_sigma_obs_tow);        // Tow effect observtaion error.
+   Type a_tow                = exp(log_a_tow);                // Tow effect shape correlation parameter.
+   Type rho_tow              = exp(log_rho_tow);              // Tow effect range correlation parameter.
+   Type sigma_process_global = exp(log_sigma_process_global); // Global process standard error .
+   Type sigma_process        = exp(log_sigma_process);        // Process standard error.
+   Type sigma_obs            = exp(log_sigma_obs);            // Observation standard error.
+   Type sigma_outlier        = exp(log_sigma_outlier);        // Outlier standard error.
+   Type rho                  = exp(log_rho);                  // Range correlation parameter.
+
+   // Number of observations:
+   int n_obs = width.size();
+   int n = lambda.rows();
+   int n_tow = tow_effect.size();
+
+   // Initialize negative log-likelihood accumulator:
+   Type res = 0;
+
+   // Proportion of outliers:
+   Type p_outlier = Type(1) / (Type(1) + exp(-logit_p_outlier));
+
+   // Rational quadratic spatial prior over tow effects:
+   matrix<Type> Sigma_tow(n_tow,n_tow);
+   for (int i = 0; i < n_tow; i++){
+      Sigma_tow(i,i) = sigma_process_tow + sigma_obs_tow;
+      for (int j = 0; j < i; j++){
+         Type d = sqrt((x[i]-x[j]) * (x[i]-x[j]) + (y[i]-y[j]) * (y[i]-y[j])) / rho_tow;
+         Sigma_tow(i,j) = sigma_process_tow * exp(-a_tow * log(1 + (d * d) / (2 * a_tow)));
+         Sigma_tow(j,i) = Sigma_tow(i,j);
+      }
+   }
+   using namespace density;
+   MVNORM_t<Type> nll_tow(Sigma_tow);
+   res += nll_tow(tow_effect);
+
+   // Rational quadratic covariance matrix for wing spread times series:
+   matrix<Type> Sigma(n,n);
+   Type a = exp(log_a);
+   Type d;
+   for (int i = 0; i < n; i++){
+      Sigma(i,i) = Type(1);
+      for (int j = 0; j < i; j++){
+         d = abs(Type(i-j)) / rho;
+         Sigma(i,j) = exp(-a * log(1 + (d * d) / (2 * a)));
+         Sigma(j,i) = Sigma(i,j);
+      }
+   }
+   using namespace density;
+   MVNORM_t<Type> nll_exp(Sigma);
+
+   // Global Gaussian process:
+   res += nll_exp(lambda_global);
+
+   // Gaussian process by tow:
+   for (int j = 0; j < n_tow; j++){
+      res += nll_exp(vector<Type>(lambda.col(j)));
+   }
+
+   // Process model for reference vessel:
+   matrix<Type> mu(n,n_tow);
+   for (int i = 0; i < n; i++){
+      for (int j = 0; j < n_tow; j++){
+         mu(i,j) = alpha + tow_effect[j] + sigma_process_global * lambda_global[i] + sigma_process * lambda(i,j);
+      }
+   }
+
+   // Observation model:
+   for (int i = 0; i < n_obs; i++){
+      res -= log((Type(1)-p_outlier) * dnorm(width[i], mu(t[i], tow[i]), sigma_obs, false) +
+                          p_outlier  * dnorm(width[i], mu(t[i], tow[i]), sigma_obs + sigma_outlier, false));
+   }
+
+   // Calculate summary statistics:
+   vector<Type> swept_area(n_tow);
+   swept_area.fill(0);
+   for (int i = 0; i < n; i++){
+      for (int j = 0; j < n_tow; j++){
+         swept_area[j] += speed(i,j) * mu(i,j);
+      }
+   }
+
+   // Export results:
+   REPORT(swept_area);
+
+   return res;
+}
