10c77b69f5a32efb8e583c2a1a7fc1c910d7b5ba
TobieSurette <tobie.surette@gmail.com>
2020-10-27 13:21:17 -0300

    Update kriging single year map.R

--- programs/figures/kriging single year map.R
+++ programs/figures/kriging single year map.R
@@ -23,26 +23,27 @@ s[categories] <- 1000000 * s[categories] / repvec(s$swept.area, ncol = length(ca
 # Perform kriging:
 m <- ked(s, variables = categories, variogram.average = 3, lag = 3, max.distance = 75)
 
-
+# Interpolation grid:
+x <- seq(-66.5, -60, len = 400)
+y <- seq(45, 49, len = 400)
 for (i in 1:length(m$variables)){
    dev.new()
 
+   # Background map:
    map.new(xlim = c(-66.5, -60-1/6), ylim = c(45.5-1/6, 48.5+5/6))
-
    bathymetry()
 
-   xx <- seq(-66.5, -60, len = 400)
-   yy <- seq(45, 49, len = 400)
+   # Interpolate:
+   index <- !is.na(m$map.longitude) & !is.na(m$map.latitude) & !is.na(m$map[,,i])
    zz <- akima::interp(x = m$map.longitude[index],
                        y = m$map.latitude[index],
                        z = m$map[,,i][index],
-                       xo = xx, yo = yy,  linear = TRUE, extrap = TRUE, duplicate = "mean")$z
+                       xo = x, yo = y,
+                       linear = TRUE, extrap = TRUE, duplicate = "mean")$z
 
-   xxx <- repvec(xx, nrow = length(yy))
-   yyy <- repvec(yy, ncol = ncol(xxx))
-   xxx <- t(xxx)
-   yyy <- t(yyy)
-   index <- in.polygon(as.polygon(p$gulf$longitude, p$gulf$latitude), xxx, yyy)
+   xx <- t(repvec(x, nrow = length(y)))
+   yy <- t(repvec(y, ncol = length(x)))
+   index <- in.polygon(as.polygon(p$gulf$longitude, p$gulf$latitude), xx, yy)
    dim(index) <- dim(zz)
    zz[!index] <- NA
 
@@ -55,93 +56,49 @@ for (i in 1:length(m$variables)){
    if (m$variables[i] == "COMSC345") breaks = c(seq(0, 2000, by = 200), 10000)
    if (m$variables[i] == "MIGE56")   breaks = c(seq(0, 15000, by = 1500), 100000)
 
-   image(xx, yy, zz, add = TRUE, col = cols(length(breaks)-1), breaks = breaks)
+   # Density:
+   image(x, y, zz, add = TRUE, col = cols(length(breaks)-1), breaks = breaks)
 
-   #map.fishing.zones(species = 2526, lwd = 1)
+   # Grid lines:
+   vline(seq(-66.5, -59, by = 1/6), col = "grey75", lwd = 0.5)
+   hline(seq(45, 49.5, by = 1/6), col = "grey75", lwd = 0.5)
+
+   # Fishing zones:
+   v <- read.gulf.spatial("fishing zone vertices shp", species = 2526, region = "gulf", label = c("12", "12E", "12F", "19"))
+   v <- subset(v, label %in% c("12", "12E", "12F", "19"))
+   plot(v, add = TRUE)
+
+   # Erase corner:
+  # plot.grid("GQ22", col = "white", border = "white")
+
+#   str <- paste0("GQ", 23:59)
+ #  plot.grid(str, col = "papayawhip")
+ #  tmp <- grid.corners(str)
+ #  text(apply(tmp[, c(1,3)], 1, mean), apply(tmp[, c(2,4)], 1, mean), substr(rownames(tmp), 3, 4), cex = 0.85)
+
+ #  str <- paste0(c(paste0("G", LETTERS), paste0("H", LETTERS)), 22)
+ #  str <- str[which(substr(str, 1, 2) == "GR"):which(substr(str, 1, 2) == "HN")]
+ #  map.grid(str, col = "papayawhip")
+ #  tmp <- grid.corners(str)
+ #  text(apply(tmp[, c(1,3)], 1, mean), apply(tmp[, c(2,4)], 1, mean), substr(rownames(tmp), 1, 2), cex = 0.85)
+ #
+ #  rect(par("usr")[1] + 1/6, par("usr")[3], par("usr")[2], par("usr")[4]-1/6, border = "black", lwd = 1)
+ #  map.axis(c(1, 4))
+
+   # Legend:
+   legend <- paste0(breaks[1:(length(breaks)-1)], " - ", breaks[2:length(breaks)])
+   legend[length(legend)] <- paste(breaks[length(breaks)-1], "+")
+   if (weight) title <- "kg / km2" else "n / km2"
+   legend("bottomleft",
+          inset = c(0.035, 0.01),
+          legend = rev(legend),
+          pt.bg = rev(cols(length(breaks))),
+          pch = 22, pt.cex = 4,
+          cex = 1.25,  bg = "white", title = title)
+
    coast()
    box(lwd = 1)
+
+   if (output %in% c("pdf", "jpeg", "jpg")) dev.off()
 }
-
-xx <- seq(-66.5 + 1/6, -60, by = 1/6)
-yy <- seq(45, 49, by = 1/6)
-for (i in 1:length(xx)) lines(c(xx[i], xx[i]), c(45, 49+1/6), col = "grey75", lwd = 0.5)
-for (i in 1:length(yy)) lines(c(-66.5+1/6, -60), c(yy[i], yy[i]),  col = "grey75", lwd = 0.5)
-
-# Draw fishing zone lines without drawing the shoreline:
-data("fishing.zone.polygons")
-p <- subset(fishing.zone.polygons, species = 2526)
-for (i in 1:length(p)){
-   for (j in 1:length(p[[i]])){
-      if ("x" %in% names(p[[i]][[j]])){
-         xx <- p[[i]][[j]]$x
-         yy <- p[[i]][[j]]$y
-         dd <- depth(xx, yy)
-         index <- which(depth(xx, yy) < -30) # Threshold depth for crab zone lines.
-         if (length(index) > 0){
-            for (k in 1:length(index)){
-               if (index[k] > 1) lines(xx[(index[k]-1):index[k]], yy[(index[k]-1):index[k]], col = "grey10", lwd = 1)
-               if (index[k] < length(xx)) lines(xx[index[k]:(index[k]+1)], yy[index[k]:(index[k]+1)], col = "grey10", lwd = 1)
-            }
-         }
-      }
-   }
-}
-
-# Erase corner:
-map.grid("GQ22", col = "white", border = "white")
-
-str <- paste0("GQ", 23:59)
-map.grid(str, col = "papayawhip")
-tmp <- grid.corners(str)
-text(apply(tmp[, c(1,3)], 1, mean), apply(tmp[, c(2,4)], 1, mean), substr(rownames(tmp), 3, 4), cex = 0.85)
-
-str <- paste0(c(paste0("G", LETTERS), paste0("H", LETTERS)), 22)
-str <- str[which(substr(str, 1, 2) == "GR"):which(substr(str, 1, 2) == "HN")]
-map.grid(str, col = "papayawhip")
-tmp <- grid.corners(str)
-text(apply(tmp[, c(1,3)], 1, mean), apply(tmp[, c(2,4)], 1, mean), substr(rownames(tmp), 1, 2), cex = 0.85)
-
-rect(par("usr")[1] + 1/6, par("usr")[3], par("usr")[2], par("usr")[4]-1/6, border = "black", lwd = 1)
-map.axis(c(1, 4))
-
-#str <- paste0(breaks[1:(length(breaks)-1)], " - ", breaks[2:length(breaks)])
-#str[length(str)] <- paste(breaks[length(breaks)-1], "+")
-
-#plot(c(0, 1), c(0, 1), type = "n", axes = FALSE, xlab = "", ylab = "")
-#a <- legend("center",
-#       legend = rev(str),
-#       pt.bg = rev(cols(length(breaks))),
-#       pch = 22,
-#       pt.cex = 3,
-#       cex = 0.95, bty = "n", plot = FALSE)
-#legend("center",
-#       legend = rev(str),
-#       pt.bg = rev(cols(length(breaks))),
-#       pch = 22,
-#       pt.cex = 3,
-#       cex = 0.95, bty = "n")
-#
-str <- paste0(breaks[1:(length(breaks)-1)], " - ", breaks[2:length(breaks)])
-str[length(str)] <- paste(breaks[length(breaks)-1], "+")
-#
-legend("bottomleft",
-       inset = c(0.035, 0.01),
-       legend = rev(str),
-       pt.bg = rev(cols(length(breaks))),
-       pch = 22,
-       pt.cex = 4,
-       cex = 1.25,  bg = "white", title = "n / km2")
-
-#points(a$rect$left + 0.5 * a$rect$w, a$rect$top - a$rect$h)
-#str <- "# / km2"
-#if (var %in% c("COM", "COMSC345")) str <- "kg / km2"
-#text(a$rect$left + 0.5 * a$rect$w, a$rect$top - a$rect$h + 0.04, str, pos = 1, cex = 1.25)
-
-if (output %in% c("pdf", "jpeg", "jpg")) dev.off()
-
-
-
-
-
-
 
