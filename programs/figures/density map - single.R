1d6180b041bbf909109f2e94d6a6ae2b573f5265
Tobie Surette <tobie.surette@gmail.com>
2020-10-28 07:13:32 -0300

    Density maps and file renaming

--- programs/figures/density map - single.R
+++ programs/figures/density map - single.R
@@ -3,11 +3,16 @@ library(gulf.stats)
 library(gulf.graphics)
 library(gulf.spatial)
 
-# Survey year:
-year <- 2020
-output <- "results/tables/"
-categories <- c("COM", "COMSC12", "COMSC345") # Define catch categories.
-weight <- TRUE
+# Program options:
+year <- 2020                                 # Survey year.
+output <- "pdf"                              # Output format.
+language <- language("en")                   # Output language.
+path <- paste0("results/figures/", language, "/maps") # File output path.
+categories <- c("COM","COMSC12","COMSC345")  # Define catch categories.
+#categories <- c("MIGE34L45", "MIGE56L69", "MIGE69L83", "MIGE83", "MIGE83L98", "MIGE83L98SC345", "FI", "FIGNO", "FM", "FP", "FMULT")
+weight <- TRUE                              # Whether to convert counts to weights.
+
+categories <- categories[1]
 
 # Load kriging polygons:
 p <- read.gulf.spatial("kriging polygons revised")["gulf"]
@@ -27,10 +32,13 @@ m <- ked(s, variables = categories, variogram.average = 3, lag = 3, max.distance
 x <- seq(-66.5, -60, len = 400)
 y <- seq(45, 49, len = 400)
 for (i in 1:length(m$variables)){
-   dev.new()
+   # Prepare output graphics device:
+   file <- paste("density map -", tolower(category(m$variables[i], language = language)), "-", year)
+   file <- paste0(path, "/", file)
+   gdevice(output, file = file)
 
    # Background map:
-   map.new(xlim = c(-66.5, -60-1/6), ylim = c(45.5-1/6, 48.5+5/6))
+   map.new(xlim = c(-66.5, -60-1/6), ylim = c(45.5, 49+1/6))
    bathymetry()
 
    # Interpolate:
@@ -49,12 +57,14 @@ for (i in 1:length(m$variables)){
 
    cols <- colorRampPalette(c("blue4", "blue", "mediumturquoise", "yellow", "orange", "red", "darkred"))
 
+   # Rescale weights:
    if (weight) zz <- 1000 * zz
 
+   breaks = c(seq(0, 15000, by = 1500), 100000)
    if (m$variables[i] == "COM")      breaks = c(seq(0, 3000, by = 300), 10000)
-   if (m$variables[i] == "COMSC12")  breaks = c(seq(0, 2000, by = 200), 10000)
-   if (m$variables[i] == "COMSC345") breaks = c(seq(0, 2000, by = 200), 10000)
-   if (m$variables[i] == "MIGE56")   breaks = c(seq(0, 15000, by = 1500), 100000)
+   if (m$variables[i] %in% c("COMSC12", "COMSC345"))  breaks = c(seq(0, 2000, by = 200), 10000)
+   if (substr(m$variables[i],1,2) == "MI") breaks = c(seq(0, 15000, by = 1500), 100000)
+   if (substr(m$variables[i],1,1) == "F") breaks = c(seq(0, 30000, by = 3000), 100000)
 
    # Density:
    image(x, y, zz, add = TRUE, col = cols(length(breaks)-1), breaks = breaks)
@@ -68,36 +78,39 @@ for (i in 1:length(m$variables)){
    v <- subset(v, label %in% c("12", "12E", "12F", "19"))
    plot(v, add = TRUE)
 
+   # Coastline:
+   coast(col = "grey75")
+
    # Erase corner:
-  # plot.grid("GQ22", col = "white", border = "white")
-
-#   str <- paste0("GQ", 23:59)
- #  plot.grid(str, col = "papayawhip")
- #  tmp <- grid.corners(str)
- #  text(apply(tmp[, c(1,3)], 1, mean), apply(tmp[, c(2,4)], 1, mean), substr(rownames(tmp), 3, 4), cex = 0.85)
-
- #  str <- paste0(c(paste0("G", LETTERS), paste0("H", LETTERS)), 22)
- #  str <- str[which(substr(str, 1, 2) == "GR"):which(substr(str, 1, 2) == "HN")]
- #  map.grid(str, col = "papayawhip")
- #  tmp <- grid.corners(str)
- #  text(apply(tmp[, c(1,3)], 1, mean), apply(tmp[, c(2,4)], 1, mean), substr(rownames(tmp), 1, 2), cex = 0.85)
- #
- #  rect(par("usr")[1] + 1/6, par("usr")[3], par("usr")[2], par("usr")[4]-1/6, border = "black", lwd = 1)
- #  map.axis(c(1, 4))
+   plot.grid("GR22", col = "white", border = "white") # Erase corner.
+   str <- paste0("GR", 23:59)
+   plot.grid(str, label = substr(str,3,4), col = "grey90", cex = 0.70)
+   str <- paste0(c(paste0("G", LETTERS), paste0("H", LETTERS)), 22)
+   str <- str[which(substr(str, 1, 2) == "GS"):which(substr(str, 1, 2) == "HN")]
+   plot.grid(str, label = substr(str,1,2), col = "grey90", cex = 0.70)
+   rect(par("usr")[1] + 1/6, par("usr")[3], par("usr")[2], par("usr")[4]-1/6, border = "black", lwd = 1)
 
    # Legend:
    legend <- paste0(breaks[1:(length(breaks)-1)], " - ", breaks[2:length(breaks)])
    legend[length(legend)] <- paste(breaks[length(breaks)-1], "+")
-   if (weight) title <- "kg / km2" else "n / km2"
+   if (weight) title <- "kg / km2" else title <- "n / km2"
    legend("bottomleft",
           inset = c(0.035, 0.01),
           legend = rev(legend),
           pt.bg = rev(cols(length(breaks))),
-          pch = 22, pt.cex = 4,
-          cex = 1.25,  bg = "white", title = title)
+          pch = 22, pt.cex = 3,
+          cex = 1.00,  bg = "white", title = title)
 
-   coast()
-   box(lwd = 1)
+   # Draw axes:
+   map.axis(c(1,4))
+
+   # Title:
+   mtext(category(m$variables[i], language = language), 3, 1.00, cex = 1.25)
+
+   # Info and stats:
+   text(par("usr")[1] + 0.9 * diff(par("usr")[1:2]),
+        par("usr")[3] + 0.9 * diff(par("usr")[3:4]),
+        year, cex = 1.5)
 
    if (output %in% c("pdf", "jpeg", "jpg")) dev.off()
 }
