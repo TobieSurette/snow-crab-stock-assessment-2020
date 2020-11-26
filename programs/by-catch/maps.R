e5a71a43a806fff9e0eafc1499e213d1c1a4b015
Tobie Surette <tobie.surette@gmail.com>
2020-11-26 16:11:42 -0400

    Map corrections, plus invertebrates

--- programs/by-catch/maps.R
+++ programs/by-catch/maps.R
@@ -2,10 +2,24 @@ library(gulf.data)
 library(gulf.graphics)
 library(gulf.spatial)
 
-year <- 2017
-species <- c("American plaice", "redfish unsp", "cod", "Witch flounder", "Thorny skate", "Yellowtail flounder", "white hake", "Halibut")
 format <- "pdf"
+year <- 2020
+fish <- FALSE
+if (fish){
+   species <- list("American plaice", "redfish unsp", "cod", "Witch flounder", "Thorny skate", "Yellowtail flounder", "white hake", "Halibut")
+   species.en <- gsub(" unsp", "", species(unlist(lapply(species(species), function(x) x[1]))))
+   species.fr <- gsub(" non spécifié", "", species(unlist(lapply(species(species), function(x) x[1])), language = "fr"))
+   species.fr <- paste0(toupper(substr(species.fr, 1, 1)), substr(species.fr, 2, nchar(species.fr)))
+   group <- "fish"
+}else{
+   # Invertebrates:
+   species <- list(6300, 6400, 6200, c(6100, 6110:6119, 6121, 6123, 6129, 6135), c(1823, 1827), 6600, 2521, 2527)
+   species.en <- c("Basket stars", "Brittle star", "Sea squirts", "Lesser toad crab", "Green sea urchins", "Starfish", "Sea cucumbers", "Greater toad crab")
+   species.fr <- c("Cornes de boeuf", "Ophuires", "Tuniciers", "Hyas coarctatus", "Oursin vert", "Étoiles de mer", "Cocombres de mer", "Hyas araneus")
+   group <- "invertebrates"
+}
 
+# Read survey grids:
 mif <- read.gulf.spatial(c("scs", "mif"))
 for (i in 1:length(mif)){
   tmp <- km2deg(mif[[i]]$x, mif[[i]]$y)
@@ -23,14 +37,15 @@ colour <- function(x, maximum, col = c("white", "black")){
 }
 
 clg()
-gdevice(format, file = paste0(getwd(), "/results/figures/", year, " - survey.by-catch.fish.pdf"),  height = 11, width = 8.5)
+gdevice(format, file = paste0(getwd(), "/results/figures/", year, " - survey.by-catch.", group, ".pdf"),  height = 11, width = 8.5)
 m <- kronecker(matrix(1:8, ncol = 2), matrix(1, nrow = 5, ncol = 5))
 m <- rbind(0,0,cbind(0, 0, m, 0),0,0)
 layout(m)
 par(mar = c(0,0,0,0))
 for (i in 1:length(species)){
    x <- read.scsset(year, valid = 1, survey = "regular")
-   y <- read.scscat(year, survey = "regular", species = species[i])
+   y <- read.scscat(year, survey = "regular", species = species[[i]])
+   y <- aggregate(y["weight.caught"], by = y[key(x)], sum)
    import(x, var = "weight.caught", fill = 0) <- y
 
    # Catch density (tonnes / km2):
@@ -55,8 +70,8 @@ for (i in 1:length(species)){
           pt.bg = colour(sqrt(legend), max = max(sqrt(x$density))),
           bg = "white", cex = 1,
           title = expression(paste("mt/km"^"2")))
-   text(-61.4, 48.9, species[i], cex = 1.1)
-   text(-61.4, 48.67, species(species(species[i])[1], language = "fr"), cex = 1.1)
+   text(-61.4, 48.9, species.en[i], cex = 1.1)
+   text(-61.4, 48.67, species.fr[i], cex = 1.1)
    box()
 
    if (i == 4){
