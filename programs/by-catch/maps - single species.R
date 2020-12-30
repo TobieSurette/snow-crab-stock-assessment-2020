93bd81c3bf3ab0b8b1681e140bfbc9ef2a2b218f
TobieSurette <tobie.surette@gmail.com>
2020-12-30 12:18:00 -0400

    Added fish density maps

--- programs/by-catch/maps - single species.R
+++ programs/by-catch/maps - single species.R
@@ -10,12 +10,14 @@ if (fish){
    species.en <- c("American plaice", "Redfish", "Atlantic cod", "Witch flounder", "Thorny skate", "Yellowtail flounder", "White hake", "Halibut")
    species.fr <- c("Plie canadienne", "Sébaste", "Morue franche", "Plie grise", "Raie épineuse", "Limande à queue jaune", "Merluche blanche", "Flétan Atlantique")
    group <- "fish"
+   max <- c(30, 50, 20, 10, 10, 5, 10, 10)
 }else{
    # Invertebrates:
    species <- list(6300, 6400, 6200, c(6100, 6110:6119, 6121, 6123, 6129, 6135), c(1823, 1827), 6600, 2521, 2527)
    species.en <- c("Basket stars", "Brittle star", "Sea squirts", "Lesser toad crab", "Green sea urchins", "Starfish", "Sea cucumbers", "Greater toad crab")
    species.fr <- c("Cornes de boeuf", "Ophuires", "Tuniciers", "Hyas coarctatus", "Oursin vert", "Étoiles de mer", "Cocombres de mer", "Hyas araneus")
    group <- "invertebrates"
+   max <- c(100, 100, 100, 100, 100, 100, 100, 100)
 }
 
 # Read survey grids:
@@ -30,13 +32,14 @@ for (i in 1:length(mif)){
 colour <- function(x, maximum, col = c("white", "black")){
    if (missing(maximum)) x <- x / base::max(x, na.rm = TRUE)
    if (!missing(maximum)) x <- x / maximum
+   x[x > 1] <- 1
    col <- colorRamp(col)(x) / 255
    col <- rgb(col[,1], col[,2], col[,3])
    return(col)
 }
 
-
 for(i in 1:length(species)){
+   print(species[i])
    clg()
    gdevice(format, file = paste0(getwd(), "/results/figures/", species.en[i], ".", min(years),  "-", max(years)), height = 11, width = 8.5)
    m <- kronecker(matrix(1:8, ncol = 2), matrix(1, nrow = 5, ncol = 5))
@@ -44,6 +47,7 @@ for(i in 1:length(species)){
    layout(m)
    par(mar = c(0,0,0,0))
    for (j in 1:length(years)){
+      print(years[j])
       x <- read.scsset(years[j], valid = 1, survey = "regular")
       y <- read.scscat(years[j], survey = "regular", species = species[[i]])
       y <- aggregate(y["weight.caught"], by = y[key(x)], sum)
@@ -53,7 +57,7 @@ for(i in 1:length(species)){
       x$density <- x$weight.caught / x$swept.area
       if (years[j] > 2017) x$density <- 1000 * x$density
 
-      x$colour <- colour(sqrt(x$density)) # Define catch colours.
+      x$colour <- colour(sqrt(x$density), max = sqrt(max[i])) # Define catch colours.
       map.new()
       for (k in 1:length(mif)){
          p <- as.polygon(mif[[k]]$longitude, mif[[k]]$latitude)
@@ -65,22 +69,22 @@ for(i in 1:length(species)){
       map("coast", col = "floralwhite", border = "saddlebrown", lwd = 0.4)
 
       # Draw legend:
-      legend <- seq(0, sqrt(max(x$density)), len = 6) ^ 2
+      legend <- seq(0, sqrt(max[i]), len = 6) ^ 2
       legend("bottomleft",
              legend = round(legend,1),
-             pch = 22,  pt.cex = 3,
-             pt.bg = colour(sqrt(legend), max = max(sqrt(x$density))),
-             bg = "white", cex = 1, lwd = 0.5,
+             pch = 22,  pt.cex = 2.5,
+             pt.bg = colour(sqrt(legend), max = sqrt(max[i])),
+             bg = "white", cex = 0.75, lwd = 0.4, col = "grey60",
              title = expression(paste("mt/km"^"2")))
       text(-60.9, 49, years[j], cex = 1.25)
       box()
 
-      if (i == 4){
+      if (j == 4){
          mtext("Longitude", 2, 2.0, cex = 1.25)
          mtext("Latitude", 1, 2.0, cex = 1.25)
          map.axis(1:2)
       }
-      if (i == 5) map.axis(3:4)
+      if (j == 5) map.axis(3:4)
    }
    dev.off()
 }
