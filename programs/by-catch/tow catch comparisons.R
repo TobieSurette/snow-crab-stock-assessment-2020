439dd9813c9a1880bfe5127bd732cfcdda92de71
Tobie Surette <tobie.surette@gmail.com>
2021-01-04 14:19:44 -0400

    Added by-catch ratios in pdf format

--- programs/by-catch/tow catch comparisons.R
+++ programs/by-catch/tow catch comparisons.R
@@ -1,11 +1,11 @@
 library(gulf.data)
 library(gulf.graphics)
 
-output <- "png"
+output <- "pdf"
 var <- "number.caught"
 z <- list()
 years <- 2013:2019
-species <- c(10)
+species <- c(6600, 6611)
 
 #6300 # Basketstars
 #6400 # Sea urchins
