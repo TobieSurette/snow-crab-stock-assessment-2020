7b845f54b400734f95b7d6bd84c2f4d869b15fd2
Tobie Surette <tobie.surette@gmail.com>
2021-01-11 16:41:34 -0400

    Revised survey bias table and added map to survey station drift analysis

--- programs/Mullowney drift.R
+++ programs/Mullowney drift.R
@@ -1,3 +1,7 @@
+library(gulf.data)
+library(gulf.graphics)
+library(gulf.spatial)
+
 var <- "FI"
 
 years <- 2013:2020
@@ -9,6 +13,20 @@ y <- read.scsbio(years, valid = 1, survey = "regular")
 y$tow.id <- tow.id(y)
 import(x, fill = 0) <- catch(y, category = var)
 x$n <- x[, var]
+
+# Summary of 2013 to 2020 changes:
+n <- as.numeric(substr(x$tow.id, 7, 7))
+n[is.na(n)] <- 0
+grid <- matrix(0, nrow = 355, ncol = length(years))
+for (i in 1:length(years)){
+   ix <- which((year(x) == years[i]) & !is.na(x$grid))
+   grid[x$grid[ix], i] <- n[ix]
+}
+grid <- t(apply(grid, 1, cumsum))
+
+r <- NULL
+for (i in 0:16)
+r <- cbind(r, t(t(apply(grid, 2, function(x) sum(x == i)))))
 
 # Remove 2013 alternate stations from the analysis:
 remove <- x$grid[(year(x) == 2013) & (substr(x$tow.id, 6, 6) == "A")]
@@ -31,5 +49,28 @@ mtext("Ratio moved / stationary", 2, 2.5)
 mtext("Year", 1, 2.5)
 box()
 
+# Read survey grids:
+mif <- scs.survey.grids()
+for (i in 1:length(mif)){
+   tmp <- km2deg(mif[[i]]$x, mif[[i]]$y)
+   mif[[i]]$longitude <- tmp[,1]
+   mif[[i]]$latitude <- tmp[,2]
+}
+
+clg()
+xx <- x[year(x) == 2013, ]
+map.new()
+for (i in 1:nrow(xx)){
+   if (x$moved[i]) polygon(mif[[xx$grid[i]]]$longitude, mif[[xx$grid[i]]]$latitude, col = "grey50", border = "grey80")
+   if (!x$moved[i]) polygon(mif[[xx$grid[i]]]$longitude, mif[[xx$grid[i]]]$latitude, col = "grey95", border = "grey80")
+}
+for (i in 1:length(remove)) polygon(mif[[remove[i]]]$longitude, mif[[remove[i]]]$latitude, col = NA, border = "grey80")
+map("coast", col = "floralwhite", border = "saddlebrown", lwd = 0.4)
+box()
+# Fishing zones:
+v <- read.gulf.spatial("fishing zone vertices shp", species = 2526, region = "gulf", label = c("12", "12E", "12F", "19"))
+v <- subset(v, label %in% c("12", "12E", "12F", "19"))
+plot(v, add = TRUE)
+map.axis(1:2)
 
 
