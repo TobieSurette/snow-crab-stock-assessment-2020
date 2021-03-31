4ced8c8057fa9e269f95f948302aab4364244e78
TobieSurette <tobie.surette@gmail.com>
2021-03-31 10:22:34 -0300

    Moved data to other locations

--- data/build/esonar.R
+++
@@ -1,8 +0,0 @@
-library(gulf.utils)
-library(gulf.data)
-
-source("C:/Users/SuretteTJ/Desktop/gulf.data/R/esonar.R")
-
-files <- locate.esonar(path = "W:/Crab/Offshore Crab Common/Fishing Year 2020/Trawl Data/South Western Gulf/ESonar/Summary", remove = FALSE)
-
-file.copy(from = files, to = "data/esonar", overwrite = FALSE, copy.date = TRUE)
